8928ab3b0ce18c18119e102963a88a96388b2a9c
Mahamud Hasan <mahamud15-10467@diu.edu.bd>
2022-01-08 15:21:09 +0600

    first commit

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  env: {
+    DB_URI: "mongodb+srv://shakil51298:Webdeveloper21mongo@cluster0.bptoi.mongodb.net/mahamuds_boighor?retryWrites=true&w=majority",
+    NEXTAUTH_URL:"https://boighor.vercel.app"
+  },
+  reactStrictMode: true,
+  images: { domains: ['res.cloudinary.com'] },
+};
